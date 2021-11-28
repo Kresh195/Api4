78daeda3bbf67c0ef133ac18838d3be44f591760
Kresh195 <KreshF5@mail.ru>
2021-11-28 19:10:15 +0700

    reformat code style

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -6,21 +6,32 @@ from dotenv import load_dotenv
 import time
 
 
-def fetch_file_format(nasa_link_image):
+def fetch_file_info(nasa_link_image):
     path_from_link = urlparse(nasa_link_image).path
     file_name = os.path.split(path_from_link)[1]
     name, file_format = os.path.splitext(file_name)
     return file_format, name
 
 
-def fetch_nasa_images(nasa_link_image):
-    file_format, name = fetch_file_format(nasa_link_image)
-    path = 'NASA_images/{}{}'.format(name, file_format)
-    download_image(nasa_link_image, path)
+def fetch_nasa_images(nasa_token):
+    payload = {"count": "30", "api_key": nasa_token}
+    nasa_links_response = requests.get(
+        'https://api.nasa.gov/planetary/apod', params=payload)
+    nasa_links_response.raise_for_status()
+    nasa_links = nasa_links_response.json()
+    for nasa_link in nasa_links:
+        nasa_link_image = nasa_link['hdurl']
+        file_format, name = fetch_file_info(nasa_link_image)
+        path = 'NASA_images/{}{}'.format(name, file_format)
+        download_image(nasa_link_image, path)
 
 
-def fetch_epic_images(epic_images_info, nasa_token):
+def fetch_epic_images(nasa_token):
     payload = {"api_key": nasa_token}
+    epic_links_response = requests.get(
+        'https://api.nasa.gov/EPIC/api/natural', params=payload)
+    epic_links_response.raise_for_status()
+    epic_images_info = epic_links_response.json()[:5]
     for epic_image_info in epic_images_info:
         date = time.strftime('%Y/%m/%d', time.strptime(epic_image_info['date'].split(' ')[0], '%Y-%m-%d'))
         print(date)
@@ -33,20 +44,8 @@ def fetch_epic_images(epic_images_info, nasa_token):
 def main():
     load_dotenv()
     nasa_token = os.getenv('NASA_TOKEN')
-    payload = {"count": "30", "api_key": nasa_token}
-    nasa_links_response = requests.get(
-        'https://api.nasa.gov/planetary/apod', params=payload)
-    nasa_links_response.raise_for_status()
-    payload = {"api_key": nasa_token}
-    epic_links_response = requests.get(
-        'https://api.nasa.gov/EPIC/api/natural', params=payload)
-    epic_links_response.raise_for_status()
-    nasa_links = nasa_links_response.json()
-    epic_images_info = epic_links_response.json()[:5]
-    for nasa_link in nasa_links:
-        nasa_link_image = nasa_link['hdurl']
-        fetch_nasa_images(nasa_link_image)
-    fetch_epic_images(epic_images_info, nasa_token)
+    fetch_nasa_images(nasa_token)
+    fetch_epic_images(nasa_token)
 
 
 if __name__ == '__main__':
