6e2517d7c11e2dfafe330f3cf14e018e37a7860e
Kresh195 <KreshF5@mail.ru>
2021-12-12 17:42:13 +0700

    Minor fixes
    Minor fixes in names and code style

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import time
 
 
-def fetch_file_info(nasa_link_image):
+def fetch_file(nasa_link_image):
     path_from_link = urlparse(nasa_link_image).path
     file_name = os.path.split(path_from_link)[1]
     name, file_format = os.path.splitext(file_name)
@@ -14,30 +14,30 @@ def fetch_file_info(nasa_link_image):
 
 
 def fetch_nasa_images(nasa_token):
-    payload = {"count": "30", "api_key": nasa_token}
+    payload = {'count': '30', 'api_key': nasa_token}
     nasa_links_response = requests.get(
         'https://api.nasa.gov/planetary/apod', params=payload)
     nasa_links_response.raise_for_status()
     nasa_links = nasa_links_response.json()
     for nasa_link in nasa_links:
         nasa_link_image = nasa_link['hdurl']
-        file_format, name = fetch_file_info(nasa_link_image)
+        file_format, name = fetch_file(nasa_link_image)
         path = 'NASA_images/{}{}'.format(name, file_format)
         download_image(nasa_link_image, path)
 
 
 def fetch_epic_images(nasa_token):
-    payload = {"api_key": nasa_token}
+    payload = {'api_key': nasa_token}
     epic_links_response = requests.get(
         'https://api.nasa.gov/EPIC/api/natural', params=payload)
     epic_links_response.raise_for_status()
-    epic_images_info = epic_links_response.json()[:5]
-    for epic_image_info in epic_images_info:
-        date = time.strftime('%Y/%m/%d', time.strptime(epic_image_info['date'].split(' ')[0], '%Y-%m-%d'))
-        print(date)
+    epic_images = epic_links_response.json()[:5]
+    for epic_image in epic_images:
+        date = epic_image['formatted_date'].split(' ')[0]
+        formatted_date = time.strftime('%Y/%m/%d', time.strptime(date, '%Y-%m-%d'))
         epic_image_link = 'https://api.nasa.gov/EPIC/archive/natural/{}/png/{}.png'.format(
-            date, epic_image_info['image'])
-        path = 'Epic_images/{}.png'.format(epic_image_info['image'])
+            formatted_date, epic_image['image'])
+        path = 'Epic_images/{}.png'.format(epic_image['image'])
         download_image(epic_image_link, path, payload=payload)
 
 
--- main.py
+++ main.py
@@ -17,12 +17,16 @@ def main():
     tg_token = os.getenv('TG_TOKEN')
     bot = telegram.Bot(token=tg_token)
     tg_chat_id = os.getenv('TG_CHAT_ID')
+
     nasa_path = 'NASA_images/'
     nasa_images = listdir(nasa_path)
+
     spacex_path = 'Spacex_images/'
     spacex_images = listdir(spacex_path)
+
     epic_path = 'Epic_images/'
     epic_images = listdir(epic_path)
+
     while True:
         send_images(nasa_images, nasa_path, bot, tg_chat_id)
         send_images(spacex_images, spacex_path, bot, tg_chat_id)
